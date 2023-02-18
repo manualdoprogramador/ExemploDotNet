8ce10575b32e942b517752e523c62718b8afc519
Gustavo Barbosa <gustavobarbosa@MacBook-Air-de-Gustavo.local>
2023-02-18 14:40:48 -0300

    adicionado um exemplo do Dynamic Expando

---
+++ ExpandoObjectDynamic/ExpandoObjectDynamic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Dynamic;
+using Newtonsoft.Json;
+
+dynamic expandDynamic = new ExpandoObject();
+
+expandDynamic.Nome = "Pedro";
+expandDynamic.Idade = 25;
+
+Console.WriteLine(expandDynamic.Nome);
+Console.WriteLine(expandDynamic.Idade);
+Console.WriteLine("-----------------------");
+
+//Sem a conversao nao e possivel acessar campos nulos
+var json = "{\"nome\":\"Alberto\",\"email\":\"alberto@teste.com\",\"sexo\":null,\"idade\":null}";
+dynamic expandoDynamicJson = JsonConvert.DeserializeObject<ExpandoObject>(json);
+Console.WriteLine(expandoDynamicJson.email);
+
+//Apos a conversao e possivel chamar os campos que estao com valores nulos
+var data = expandoDynamicJson as IDictionary<string, object>;
+
+Console.WriteLine(data["nome"]);
+Console.WriteLine(data["idade"]);
+
+// Mas um campo que nao estao no json, nao é possível acessa-lo
+//Console.WriteLine(data["dataNascimento"]);
+
+if (data.ContainsKey("dataNascimento"))
+    Console.WriteLine(data["dataNascimento"]);
+else
+    Console.WriteLine("Campo nao informado no json");
+
+
+// Entao a vantagem que a gente tem em fazer essa conversao é que conseguimos manipular um campo mesmo
+// ele não estando no json
+
+
+
